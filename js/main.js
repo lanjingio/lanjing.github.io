f16f56dc51982e2f26e4487b5b375aa479ea2af2
lanjing <lanj163@163.com>
2019-08-15 11:41:12 +0800

    111

--- js/main.js
+++ js/main.js
@@ -13,9 +13,32 @@
         $('.title').css('color',getColor());
     }, 1000);
 
+    //加载留言
+    $.ajax({
+        url:'http://localhost:3000/getMsg',
+        success:function (backData) {
+            const resHTML = template('template',backData);
+            $('.list').html(resHTML);
+        }
+    });
     // 写留言
-    // $('.sayNote').on('click',()=>{
-    //     e.preventDefault();
-    //     $('#myModal').show();
-    // });
+    $('#submit').on('click',()=>{
+        let fd = new FormData($('form')[0]);
+        $.ajax({
+            type:'post',
+            url:'http://localhost:3000/addMsg',
+            data:fd,
+            processData: false,
+            contentType: false,
+            success:function (backData) {
+                if (backData.code == 200) {
+                    alert(backData.msg);
+                }else {
+                    alert(backData.msg);
+                }
+                //模态框隐藏
+                $('#myModal').modal('hide');
+            }
+        });
+    });
 }());
